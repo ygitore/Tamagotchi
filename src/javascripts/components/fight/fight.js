21f563bbfc493633c771f9332c8cccb1d50a30ca
Yitbarek Gitore <yitbareky@gmail.com>
2019-10-25 16:15:29 -0500

    finished fight quadrant

--- src/javascripts/components/fight/fight.js
+++ src/javascripts/components/fight/fight.js
@@ -1,16 +1,17 @@
 import util from '../../helpers/utilities';
 import './fight.scss';
 
+let strength = 100;
 const printFight = () => {
   let domString = '';
   domString += `<div class="fight">Fight</div>
                 <div class="fightContainer">
                   <div class="btn-container">
                     <div>
-                      <button class='btn-braverly'>Bravely</button>
+                      <button id = "btn-braverly" class='btn-braverly'>Bravely</button>
                     </div>
                     <div>
-                      <button class="violence">Violence</button>
+                      <button id = "violence" class="violence">Violence</button>
                     </div>
                   </div>
                   <div class="fight-progress-container">
@@ -21,5 +22,39 @@ const printFight = () => {
                  </div>`;
   util.printToDom('fight', domString);
 };
-
-export default { printFight };
+const committingViolence = () => {
+  const progBar = document.getElementById('fight-progress-increase');
+  if (strength <= 0) {
+    strength = 0;
+  } else {
+    strength -= 10;
+    progBar.style.borderBottomRightRadius = '0px';
+    progBar.style.borderTopRightRadius = '0px';
+  }
+  progBar.textContent = `${strength}%`;
+  progBar.style.width = `${strength}%`;
+};
+const runningAway = () => {
+  const progBar = document.getElementById('fight-progress-increase');
+  progBar.style.borderBottomRightRadius = '15px';
+  progBar.style.borderTopRightRadius = '15px';
+  progBar.textContent = `${strength}%`;
+  if (strength >= 100) {
+    strength = 100;
+  } else {
+    strength += 1;
+    if (strength >= 100) {
+      strength = 100;
+    } else {
+      progBar.style.borderBottomRightRadius = '0px';
+      progBar.style.borderTopRightRadius = '0px';
+    }
+  }
+  progBar.textContent = `${strength}%`;
+  progBar.style.width = `${strength}%`;
+};
+const fightAttachments = () => {
+  document.getElementById('btn-braverly').addEventListener('click', runningAway);
+  document.getElementById('violence').addEventListener('click', committingViolence);
+};
+export default { printFight, fightAttachments };
